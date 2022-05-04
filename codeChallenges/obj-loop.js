2041cefa27a46817a4dab4cbb120808f163d497c
pamelagilmour <65507722+pamelagilmour@users.noreply.github.com>
2022-05-04 10:47:35 -0700

    Add a different solution

--- codeChallenges/obj-loop.js
+++ codeChallenges/obj-loop.js
@@ -21,18 +21,35 @@ let thing1 = [
 
 // Loop array 'thing1' and output the fields in a table form
 
-// const ids = thing1.map(ofThing => {
-// 	// return ofThing.id + ofThing.type + ;
-// })
+let topRow = Object.keys(thing1[0]);
+let row1 = [];
+let row2 = [];
+let row3 = [];
+let row4 = [];
 
-// console.log(ids);
+
+for (i = 0; i < thing1.length; i++) {
+	row1.push(thing1[i].id);
+	row2.push(thing1[i].type);
+	row3.push(thing1[i].color);
+	row4.push(thing1[i].count);
+
+
+}
+	console.log(topRow);
+	console.log(row1);
+	console.log(row2);
+	console.log(row3);
+	console.log(row4);
+
+// let rowStr = '';
+// const ids = thing1.map(ofThing1 => { })
 
 // for (let index in thing1) {
-// 	thing1.map(ofThing => {
-// 	return ofThing.type;
+// 	thing1.map(ofThing1 => {
+// 	return ofThing.type
 // 	})
 // }
-
 
 
 // Sum the field 'count' and output the value
@@ -47,4 +64,13 @@ for (i = 0; i < thing1.length; i++) {
 
 console.log(sum);
 
+
 // Add a row at the beginning of the output with the field names
+
+for ( let property in thing1) {
+	// if (Object.getOwnPropertyNames(thing1)) {
+		// let value = thing1[property];
+		// console.log(Object.keys(thing1[0]));
+	// }
+	Object.keys(thing1[0]);
+}
